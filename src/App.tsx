4dc1da9c7fbd496311292df21708ac6d7a9f66b1
Arthur Lima <arthurlima121213@gmail.com>
2025-05-11 17:15:30 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import {Header} from './Components/Header/Header'
+function App() {
+  return (
+    <div>
+      <Header />
+      <ul>
+        <li>Task 1</li>
+        <li>Task 2</li>
+        <li>Task 3</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
